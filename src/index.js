3f201c6ba76663b9d4f48698b78aebd51f6c86f1
waleedalfatihyahya <59061014+waleedalfatihyahya@users.noreply.github.com>
2022-11-02 08:34:36 +0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+import "./assets/css/index.css";
+import { UsersProvider } from "context/usersContext";
+import { SocketProvider } from "context/socketContext";
+
+ReactDOM.render(
+	<React.StrictMode>
+		<SocketProvider>
+			<UsersProvider>
+				<App />
+			</UsersProvider>
+		</SocketProvider>
+	</React.StrictMode>,
+	document.getElementById("root")
+);
+
+
+reportWebVitals();
